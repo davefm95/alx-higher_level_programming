eeb149e8e35c6706034f223391fa7a2b0c2d6622
Femi Adetoye <davefm95@gmail.com>
2023-01-09 10:13:40 -0800

    added 5and 6

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    lst = [letter for letter in my_string]
-    for i in lst:
-        if ord(i) == ord('c') or ord(i) == ord('C'):
-            lst.remove(i)
-    new_str = "".join(lst)
+    new_str = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_str += char
     return new_str
