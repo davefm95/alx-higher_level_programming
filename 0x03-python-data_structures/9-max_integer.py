df0f9ea25566f0ab00ede23cbe5f6e7a4cde659c
Femi Adetoye <davefm95@gmail.com>
2023-01-09 08:21:57 -0800

    addee 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        max_ = None
+    else:
+        max_ = my_list[0]
+        for num in my_list:
+            if num > max_:
+                max_ = num
+    return max_
