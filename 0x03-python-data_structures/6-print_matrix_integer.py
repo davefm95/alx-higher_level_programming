00521e91ea2bd759cdb44061fc4a2f6aceba46f4
Femi Adetoye <davefm95@gmail.com>
2023-01-09 09:47:04 -0800

    y

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
         for row in matrix:
-                for i in range(len(row)):
-                        if i != len(row) - 1:
-                                print("{:d}".format(row[i]), end=" ")
-                        else:
-                                print("{:d}".format(row[i]))
+                for num in row:
+                        print("{:d}".format(num), end='')
+                        if num != row[-1]:
+                                print(" ", end='')
+                print("")
