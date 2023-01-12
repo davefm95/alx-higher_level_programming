c77a56c47070850ceb314e4c0799a36f00d1b247
Femi Adetoye <davefm95@gmail.com>
2023-01-11 18:01:46 -0800

    added 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,14 +2,14 @@
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
-    rom_val = dict(I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000)
+    rom_val = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     roman_string = roman_string.upper()
     subs = 0
     adds = 0
     used = 0
     if len(roman_string) == 1:
         return rom_val[roman_string[0]]
-    st_list = [rom_val[l] for l in roman_string]
+    st_list = [rom_val[let] for let in roman_string]
     for i in range(len(st_list)):
         if used == 1:
             used = 0
