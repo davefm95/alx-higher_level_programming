bf1ad70b40c4106d8ce075cd2961d28be6f91758
Femi Adetoye <davefm95@gmail.com>
2023-01-23 08:14:08 -0800

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    num = 0
     for item in range(x):
         try:
-            print("{}".format(my_list[item]), end="")
+            print(my_list[item], end='')
+            num += 1
         except Exception:
-            print("")
-            return item
-    print("")
-    return item + 1
+            break
+    print('')
+    return num
