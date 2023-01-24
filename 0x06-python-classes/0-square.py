46b4be9f7f993fa470f505a914f6d2c678c054c6
Femi Adetoye <davefm95@gmail.com>
2023-01-24 07:11:59 -0800

    added 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" A module that defines an empty square class.
+"""
 class Square:
-    def __init__(self):
-        pass
+    """Represents a four sided quadrilateral with equal sides
+    """
+    pass
