cdb5711184ee44758ddd1956ea1c778507a1f52b
Femi Adetoye <davefm95@gmail.com>
2023-01-24 08:14:41 -0800

    added 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This module defins a class square
+"""
+
+
+class Square():
+    """Square class definition
+    """
+    def __init__(self, size=0):
+        """Initializes a square with given size
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Return s the size of fhe square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets the size of the square
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Returns the area of the square
+        """
+        return self.__size * self.__size
