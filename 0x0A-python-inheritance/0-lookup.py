a50b713de2700603bc160a355edf807e7bd07230
Femi Adetoye <davefm95@gmail.com>
2023-02-06 09:55:26 -0800

    a1;2cdded 0y

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains the lookup function
+"""
+
+
+def lookup(obj):
+    """Returns list of all attributes of an object"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+This file contains information about the 0x0A-python-inheritance project
