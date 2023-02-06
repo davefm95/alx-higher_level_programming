72d0bbbbb75bf46931277614f22c6a23bae3279d
Femi Adetoye <davefm95@gmail.com>
2023-02-06 11:16:34 -0800

    added y

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """This func checks if ob is a subclass of a_class"""
-    if issubclass(type(obj), a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This the 6-base_geometry module"""
+
+
+class BaseGeometry:
+    """This class defines a base geometry object"""
+    def area(self):
+        """This function raises an exception"""
+        raise Exception("area() is not implemented")
