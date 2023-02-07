900e687cecd07fa5b6ad22aee2522d95855f656c
Femi Adetoye <davefm95@gmail.com>
2023-02-07 13:04:59 -0800

    added 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,6 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """This func saves a jsoj str to a file"""
-    st = json.dumps(my_obj)
     with open(filename, 'w', encoding='utf-8') as f:
-        f.write(st)
+        json.dump(my_obj, f)
