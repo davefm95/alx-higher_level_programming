89e674725c2d1ec6764961e9d1b7952315fe4ecc
Femi Adetoye <davefm95@gmail.com>
2023-02-13 17:32:41 -0800

    added

--- 0x0C-python-almost_a_circle/c.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-from models.rectangle import Rectangle
-r = Rectangle(4, 6, 3, 3, 111)
-print(r.id)
-r = Rectangle(5, 7)
-print(r.id)
--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """This module contains class base"""
+import json
 
 
 class Base:
     """This is the base class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         """This functiom inits base class"""
         if id is None:
@@ -12,3 +14,27 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns json string of argument"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """saves json string to file"""
+        if list_objs is None or len(list_objs) == 0:
+            st = "[]"
+        elif len(list_objs) != 0:
+            ld = []
+            for obj in list_objs:
+                ld.append(obj.to_dictionary())
+            st = cls.to_json_string(ld)
+        name = "Base"
+        if st != "[]":
+            name = list_objs[0].__class__.__name__
+        name += ".json"
+        with open(name, "w", encoding="utf-8") as f:
+            f.write(st)
