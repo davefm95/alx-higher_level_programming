a16338566546beb0a466d4911f211d474659e4fe
Femi Adetoye <davefm95@gmail.com>
2023-03-16 12:15:47 -0700

    added

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creqtes a uaer and grants priveleges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO user_0d_0@localhost;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_0@localhost;
