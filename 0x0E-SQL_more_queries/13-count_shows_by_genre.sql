f7783430ab363e69f4577cd87fcf02437630e37e
Femi Adetoye <davefm95@gmail.com>
2023-03-16 15:28:16 -0700

    @1;2c

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- lists genres and number of shows associated with them
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_genre.id = tv_show_genres.genre_id GROUP BY tv_genre.name ORDER BY number_of_shows DESC;
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_genre.id = tv_show_genres.genre_id GROUP BY tv_genre.name ORDER BY number_of_shows DESC;
