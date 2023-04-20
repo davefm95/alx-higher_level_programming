2f0e6015a3ec839c970dab4df95ff106dbc575a3
Femi Adetoye <davefm95@gmail.com>
2023-04-20 10:52:14 -0700

    added

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module returns a record with a name match"""
+
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    db = MySQLdb.connect(port=3306, user=argv[1],
+                         password=argv[2], database=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = %s", (argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print (row)
