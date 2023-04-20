bfc433e549ab5930f36c9fcaddd83d41e43416e8
Femi Adetoye <davefm95@gmail.com>
2023-04-20 01:24:43 -0700

    added

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """Shows all data in states table"""
+
 import MySQLdb
 from sys import argv
-name, pword, db_name = argv[1], argv[2], argv[3]
-db = MySQLdb.connect(host="localhost", port=3306, user=name, password=pword, database=db_name)
-c = db.cursor()
-c.execute("""SELECT * FROM states""")
-rows = c.fetchall()
-for row in rows:
-    print(row)
+if name == "__main__":
+    name, pword, db_name = argv[1], argv[2], argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, user=name, password=pword, database=db_name)
+    c = db.cursor()
+    c.execute("""SELECT * FROM states""")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
