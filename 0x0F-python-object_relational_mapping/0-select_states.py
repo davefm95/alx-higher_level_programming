8d8589e47a235b350eb5bea83ec3c17160f50b1e
Femi Adetoye <davefm95@gmail.com>
2023-04-20 01:31:54 -0700

    a1;2cdde

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,10 +4,11 @@
 import MySQLdb
 from sys import argv
 if __name__ == "__main__":
-    name, pword, db_name = argv[1], argv[2], argv[3]
-    db = MySQLdb.connect(host="localhost", port=3306, user=name, password=pword, database=db_name)
+    """ List ststes in table """
+    name, pwd, db_name = argv[1], argv[2], argv[3]
+    db = MySQLdb.connect(port=3306, user=name, password=pw, database=db_name)
     c = db.cursor()
-    c.execute("""SELECT * FROM states""")
+    c.execute("SELECT * FROM states")
     rows = c.fetchall()
     for row in rows:
         print(row)
