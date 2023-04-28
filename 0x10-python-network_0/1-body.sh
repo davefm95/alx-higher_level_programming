ec794b09da137f9ace1bca38949a21d96165b07a
davefm95 <davefm95@gmail.com>
2023-04-27 19:36:34 -0700

    dde

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a url requaesy to a ur and displays size
-curl -s -o - -w '%{size_download}\n' $1
+curl -sL -o - $1
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# sends delete req
+curl -sXs DELETE $1
